9a4fde49ff8335f6f5434ffbf5cda43491189d32
Roland Conybeare <rconybeare@gmail.com>
2024-09-29 13:35:36 -0500

    build: plus make-compatible fetch + verify steps.
    Using gnu m4 as example

---
+++ mk/verify-sha256.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Require:
+# 1. [state/expected.sha256] contains expected sha256 for tarball
+#
+# Promise:
+# 2. [state/verify.result] looks like:
+#    2a. succes:
+#          ok <SHA256> <TARBALL>
+#        when tarball verified against expected sha256
+#    2b. failure:
+#          err <message>
+#        when sha256 did not match expected value
+# 3. preserves [state/verify.result] file modification time
+#    when contents did not change
+
+self_name=$(basename ${0})
+
+usage() {
+    echo "$self_name"
+}
+
+set -x
+rm -f state/verify.tmp state/verify.err
+sha256sum --check state/expected.sha256 2> state/verify.err
+set +x
+
+err=$?
+
+if [[ $err -eq 0 ]]; then
+    set -x
+    echo -n "ok " > state/verify.tmp
+    cat state/expected.sha256 >> state/verify.tmp
+    set +x
+else
+    set -x
+    echo -n "err " > state/verify.tmp
+    cat state/verify.err >> state/verify.tmp
+    set +x
+fi
+
+rm -f state/verify.err
+
+if [[ -f state/verify.result ]]; then
+    set -x
+    diff -q state/verify.tmp state/verify.result
+    set +x
+    err=$?
+else
+    # no prior result to compare
+    err=1
+fi
+
+if [[ $err -ne 0 ]]; then
+    set -x
+    mv -f state/verify.tmp state/verify.result
+    set +x
+else
+    set -x
+    rm state/verify.tmp
+    set +x
+fi
