3658f2c9fe23fb57573adb88fb06682a2e8ccb4b
Roland Conybeare <rconybeare@gmail.com>
2024-09-29 14:38:48 -0500

    minor: streamline require-sha256 output

--- mk/require-sha256.sh
+++ mk/require-sha256.sh
@@ -48,22 +48,28 @@ if [[ ! -f ${tarball_path} ]]; then
 fi
 
 set -x
-
 sha256sum ${tarball_path} > state/actual.sha256
-
 echo "${sha256} ${tarball_path}" > state/tmp.sha256
+set +x
+
 
 if [[ -f state/expected.sha256 ]]; then
+    set -x
     diff state/tmp.sha256 state/expected.sha256
+    set +x
     err=$?
 else
     err=1
 fi
 
 if [[ $err -ne 0 ]]; then
+    set -x
     mv state/tmp.sha256 state/expected.sha256
+    set +x
 fi
 
+set -x
 rm -f state/tmp.sha256
+set +x
 
 # end require-sha256.sh
