fa1a61670644fd466fe59a1299320682a5370248
Roland Conybeare <rconybeare@gmail.com>
2024-09-30 07:56:31 -0500

    build: for automake: move unpack impl -> mk/unpack.mk

---
+++ mk/unpack.mk
@@ -0,0 +1,13 @@
+# Require:
+# - including makefile provides TOP_DIR, tarball_path, srcdir
+#
+# Included in per-package Makefiles ($(TOP_DIR)/pkgs/foo/Makefile)
+#
+
+state/unpack.result: state/verify.result 
+	$(TOP_DIR)/mk/unpack-tarball.sh --tarball-path=$(tarball_path) --src-dir=${srcdir}
+
+.PHONY: unpack
+unpack: state/unpack.result
+
+
