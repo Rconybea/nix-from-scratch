2682162191d14591c638003abb8fdf6b79994deb
Roland Conybeare <rconybeare@gmail.com>
2024-10-01 00:00:57 -0500

    build: modularise for pkg Makefiles, using automake as example

---
+++ mk/compile.mk
@@ -0,0 +1,12 @@
+# Require:
+# - including makefile provides TOP_DIR, builddir
+#
+# Included in per-package Makefiles ($(TOP_DIR)/pkgs/foo/Makefile)
+#
+
+state/compile.result: state/config.result
+	$(TOP_DIR)/mk/compile-autotools.sh --build-dir=$(builddir)
+
+.PHONY: compile
+compile: state/compile.result $(builddir)
+
