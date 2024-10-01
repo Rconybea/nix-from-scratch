b146d865601b5ddd4f9025f8a2d5895a0d252f43
Roland Conybeare <rconybeare@gmail.com>
2024-10-01 00:08:17 -0500

    build: use .mk ingredients to simplify m4 build

--- mk/patch.mk
+++ mk/patch.mk
@@ -4,8 +4,8 @@
 # Included in per-package Makefiles ($(TOP_DIR)/pkgs/foo/Makefile)
 #
 
-state/patch.result: state/unpack.result ${patch_script}
-	$(TOP_DIR)/mk/patch-src-dir.sh --src-dir=$(srcdir) --patch-script=${patch_script}
+state/patch.result: state/unpack.result $(patch_script)
+	$(TOP_DIR)/mk/patch-src-dir.sh --src-dir=$(srcdir) --patch-script=$(patch_script)
 
 .PHONY: patch
 patch: state/patch.result
