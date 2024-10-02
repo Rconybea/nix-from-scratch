88c5a6d0c57de8e0747b6d843089759526193c5f
Roland Conybeare <rconybeare@gmail.com>
2024-10-01 19:53:20 -0500

    + configure-extra-args feature for pkgconf

--- mk/configure.mk
+++ mk/configure.mk
@@ -1,13 +1,15 @@
 # Require:
-# - including makefile provides TOP_DIR, PREFIX, srcdir, builddir, cflags, ldflags
+# - including makefile provides TOP_DIR, PREFIX, srcdir, builddir, cflags, ldflags, configure_extra_args
 #
 # Included in per-package Makefiles ($(TOP_DIR)/pkgs/foo/Makefile)
 #
 
 state/config.result $(builddir): state/patch.result
 	$(TOP_DIR)/mk/configure-autotools.sh \
-       --prefix=$(PREFIX) --src-dir=$(srcdir) \
-       --build-dir=$(builddir) --cflags="$(cflags)" --ldflags="$(ldflags)"
+       --prefix=$(PREFIX) --src-dir=$(srcdir) --build-dir=$(builddir) \
+       --cflags="$(cflags)" --ldflags="$(ldflags)" \
+       --configure-extra-args="$(configure_extra_args)"
+
 
 .PHONY: config
 config: state/config.result
