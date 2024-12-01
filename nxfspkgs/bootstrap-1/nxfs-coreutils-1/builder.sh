baf7e63b07972eeea3fb3af1bce3f3ec3d11ac60
Roland Conybeare <rconybeare@gmail.com>
2024-12-01 14:00:37 -0500

    build: + nxfs bootstrap stage1 coreutils

--- nxfspkgs/bootstrap-1/nxfs-coreutils-1/builder.sh
+++ nxfspkgs/bootstrap-1/nxfs-coreutils-1/builder.sh
@@ -10,12 +10,21 @@ echo "head=${head}";
 echo "mkdir=${mkdir}";
 echo "nxfs_sysroot_1=${nxfs_sysroot_1}";
 echo "nxfs_coreutils_0=${nxfs_coreutils_0}";
+echo "redirect_elf_file=${redirect_elf_file}";
+echo "target_interpreter=${target_interpreter}";
+echo "target_runpath=${target_runpath}";
 echo "TMP=${TMP}"
 echo
 
 ${mkdir} ${out}
 
+# libc: only as smoke test for valid sysroot
 libc=${nxfs_sysroot_1}/lib/libc.so.6
+
+# ----------------------------------------------------------------
+# helper bash script
+
+source "${redirect_elf_file}"
 
 # ----------------------------------------------------------------
 # verify initial paths
@@ -33,13 +42,11 @@ echo "stage1 libc:          ${libc}"
 # ----------------------------------------------------------------
 # copy bash to temporary staging dir
 #
-staging=${TMP}/bash
+staging=${TMP}
 
-${mkdir} ${staging}
+${mkdir} -p ${staging}
 
 (cd ${nxfs_coreutils_0} && (${tar} cf - . | ${tar} xf - -C ${staging}))
-
-elf_magic=$'\x7fELF'
 
 ${chmod} u+w ${staging}
 ${chmod} u+w ${staging}/bin
@@ -50,35 +57,15 @@ for dir in ${staging}/bin ${staging}/libexec/coreutils; do
         echo "consider [${file}]"
 
         if [[ -f ${file} ]]; then
-            # read first 4 bytes for ELF magic
-            header=$(${head} -c 4 "${file}")
-
-            if [[ ${header} == ${elf_magic} ]]; then
-                echo "accept elf file [${file}]"
-
-                old_runpath=$(${patchelf} --print-rpath ${file})
-
-                echo "$(${basename} $file) runpath (before redirecting): ${old_runpath}"
-
-                ${chmod} u+w ${file}
-                ${patchelf} --set-rpath ${nxfs_sysroot_1}/usr/lib:${nxfs_sysroot_1}/lib ${file}
-                ${chmod} u-w ${file}
-
-                new_runpath=$(${patchelf} --print-rpath ${file})
-
-                echo "$(${basename} $file) runpath (after redirecting): ${new_runpath}"
-            else
-                echo "reject non-elf file [${file}] (doesn't begin with ELF magic)"
-            fi
+            redirect_elf_file ${file} ${target_interpreter} ${target_runpath}
         else
-            echo "reject non-regular-file [${file}]"
+            echo "skip non-regular-file [${file}]"
         fi
     done
 done
 
 ${chmod} u-w ${staging}/libexec/coreutils
 ${chmod} u-w ${staging}/bin
-${chmod} u-w ${staging}
 
 # ----------------------------------------------------------------
 # copy to final destination
--- nxfspkgs/bootstrap-1/nxfs-coreutils-1/default.nix
+++ nxfspkgs/bootstrap-1/nxfs-coreutils-1/default.nix
@@ -1,17 +1,20 @@
 let
   nxfs-coreutils-0 = import ../../bootstrap/nxfs-coreutils-0/default.nix;
-  nxfs-patchelf-0 = import ../../bootstrap/nxfs-patchelf-0/default.nix;
+  nxfs-patchelf-1 = import ../nxfs-patchelf-1/default.nix;
   nxfs-tar-0 = import ../../bootstrap/nxfs-tar-0/default.nix;
   nxfs-bash-1 = import ../nxfs-bash-1/default.nix;
   nxfs-sysroot-1 = import ../nxfs-sysroot-1/default.nix;
+  nxfs-redirect-elf-file = import ../nxfs-redirect-elf-file/default.nix;
 
   bash = "${nxfs-bash-1}/bin/bash";
   basename = "${nxfs-coreutils-0}/bin/basename";
   chmod = "${nxfs-coreutils-0}/bin/chmod";
   head = "${nxfs-coreutils-0}/bin/head";
   mkdir = "${nxfs-coreutils-0}/bin/mkdir";
-  patchelf = "${nxfs-patchelf-0}/bin/patchelf";
+  patchelf = "${nxfs-patchelf-1}/bin/patchelf";
   tar = "${nxfs-tar-0}/bin/tar";
+
+  redirect_elf_file = "${nxfs-redirect-elf-file}/bootstrap-scripts/redirect-elf-file.sh";
 in
 
 derivation {
@@ -27,10 +30,15 @@ derivation {
   patchelf = patchelf;
   tar = tar;
 
+  redirect_elf_file = redirect_elf_file;
+
   nxfs_coreutils_0 = nxfs-coreutils-0;
   nxfs_sysroot_1 = nxfs-sysroot-1;
 
   args = [./builder.sh];
+
+  target_interpreter = "${nxfs-sysroot-1}/lib64/ld-linux-x86-64.so.2";
+  target_runpath = "${nxfs-sysroot-1}/usr/lib:${nxfs-sysroot-1}/lib";
 
   buildInputs = [];
 }
