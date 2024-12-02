86bd8f6f128592f6a3d264d2e6edf5a6c2d1f5e6
Roland Conybeare <rconybeare@gmail.com>
2024-12-01 22:55:22 -0600

    nxfs-tar-1: use in-store sysroot

---
+++ nxfspkgs/bootstrap-1/nxfs-tar-1/default.nix
@@ -0,0 +1,44 @@
+let
+  nxfs-coreutils-1 = import ../nxfs-coreutils-1/default.nix;
+  nxfs-patchelf-1 = import ../nxfs-patchelf-1/default.nix;
+  nxfs-tar-0 = import ../../bootstrap/nxfs-tar-0/default.nix;
+  nxfs-bash-1 = import ../nxfs-bash-1/default.nix;
+  nxfs-sysroot-1 = import ../nxfs-sysroot-1/default.nix;
+  nxfs-redirect-elf-file = import ../nxfs-redirect-elf-file/default.nix;
+
+  bash = "${nxfs-bash-1}/bin/bash";
+  basename = "${nxfs-coreutils-1}/bin/basename";
+  chmod = "${nxfs-coreutils-1}/bin/chmod";
+  head = "${nxfs-coreutils-1}/bin/head";
+  mkdir = "${nxfs-coreutils-1}/bin/mkdir";
+  patchelf = "${nxfs-patchelf-1}/bin/patchelf";
+  tar = "${nxfs-tar-0}/bin/tar";
+
+  redirect_elf_file = "${nxfs-redirect-elf-file}/bootstrap-scripts/redirect-elf-file.sh";
+in
+
+derivation {
+  name = "nxfs-coreutils-1";
+  system = builtins.currentSystem;
+
+  bash = bash;
+  chmod = chmod;
+  basename = basename;
+  head = head;
+  mkdir = mkdir;
+  builder = bash;
+  patchelf = patchelf;
+  tar = tar;
+
+  redirect_elf_file = redirect_elf_file;
+
+  nxfs_tar_0 = nxfs-tar-0;
+  nxfs_sysroot_1 = nxfs-sysroot-1;
+
+  args = [./builder.sh];
+
+  target_interpreter = "${nxfs-sysroot-1}/lib64/ld-linux-x86-64.so.2";
+  target_runpath = "${nxfs-sysroot-1}/usr/lib:${nxfs-sysroot-1}/lib";
+
+  buildInputs = [];
+}
