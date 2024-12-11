36455f0d7dec27f0145d7b3027689a215ff75f1e
Roland Conybeare <rconybeare@gmail.com>
2024-12-11 07:25:03 -0600

    bootstrap-1-demo/hello-1: builds + runs

--- nxfspkgs/bootstrap-1-demo/hello-1/builder.sh
+++ nxfspkgs/bootstrap-1-demo/hello-1/builder.sh
@@ -6,11 +6,19 @@ echo
 echo "gcc=${gcc}";
 echo "mkdir=${mkdir}";
 echo "bash=${bash}";
+echo "toolchain=${toolchain}";
+echo "coreutils=${coreutils}";
+echo "sysroot=${sysroot}";
 echo
 
-${mkdir} ${out}
+export PATH=${toolchain}/bin:${coreutils}/bin:
 
-echo "hello roly" > ${out}/greetings.txt
+mkdir -p ${out}
+
+#echo "hello roly" > ${out}/greetings.txt
 
 ${gcc} --version
-${gcc} -o ${out}/hello ${src}
+${gcc} -o ${out}/hello -Wl,--enable-new-dtags -Wl,--rpath=${sysroot}/lib -Wl,--dynamic-linker=${sysroot}/lib/ld-linux-x86-64.so.2 ${src}
+
+# can we run it?
+${out}/hello > ${out}/greetings.txt
--- nxfspkgs/bootstrap-1-demo/hello-1/default.nix
+++ nxfspkgs/bootstrap-1-demo/hello-1/default.nix
@@ -2,8 +2,8 @@ let
   nxfs-toolchain-1 = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
   nxfs-coreutils-1 = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
   nxfs-bash-1 = import ../../bootstrap-1/nxfs-bash-1/default.nix;
+  nxfs-sysroot-1 = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
 
-  gcc = "${nxfs-toolchain-1}/bin/x86_64-pc-linux-gnu-gcc";
   mkdir = "${nxfs-coreutils-1}/bin/mkdir";
   bash = "${nxfs-bash-1}/bin/bash";
 
@@ -13,12 +13,16 @@ derivation {
   name = "hello-1";
   system = builtins.currentSystem;
 
-  gcc = gcc;
+  gcc = "x86_64-pc-linux-gnu-gcc";
   mkdir = mkdir;
   bash = bash;
 
   builder = bash;
   args = [ ./builder.sh ];
+
+  toolchain = nxfs-toolchain-1;
+  coreutils = nxfs-coreutils-1;
+  sysroot = nxfs-sysroot-1;
 
   src = ./hello.c;
 }
