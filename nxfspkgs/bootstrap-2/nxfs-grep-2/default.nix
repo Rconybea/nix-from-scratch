5834f95d919d9929f3e5b2699bf9be7cd5ffd067
Roland Conybeare <rconybeare@gmail.com>
2024-12-11 21:41:17 -0600

    + bootstrap-2/nxfs-grep-2.  Builds in nix!

---
+++ nxfspkgs/bootstrap-2/nxfs-grep-2/default.nix
@@ -0,0 +1,39 @@
+let
+  nxfs-grep-1        = import ../../bootstrap-1/nxfs-grep-1/default.nix;
+
+  nxfs-gcc-wrapper-2 = import ../nxfs-gcc-wrapper-2/default.nix;
+
+  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
+#  nxfs-sed-1         = import ../../bootstrap-1/nxfs-sed-1/default.nix;
+  nxfs-gnumake-1     = import ../../bootstrap-1/nxfs-gnumake-1/default.nix;
+  nxfs-gawk-1        = import ../../bootstrap-1/nxfs-gawk-1/default.nix;
+  nxfs-tar-1         = import ../../bootstrap-1/nxfs-tar-1/default.nix;
+  nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
+  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
+  nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
+  nxfs-bash-1        = import ../../bootstrap-1/nxfs-bash-1/default.nix;
+in
+
+derivation {
+  name         = "grep";
+  system       = builtins.currentSystem;
+
+  gnumake      = nxfs-gnumake-1;
+  bash         = nxfs-bash-1;
+  sed          = nxfs-sed-2;
+  gcc_wrapper  = nxfs-gcc-wrapper-2;
+  toolchain    = nxfs-toolchain-1;
+  sysroot      = nxfs-sysroot-1;
+  coreutils    = nxfs-coreutils-1;
+  tar          = nxfs-tar-1;
+  gawk         = nxfs-gawk-1;
+  grep         = nxfs-grep-1;
+
+  builder      = "${nxfs-bash-1}/bin/bash";
+  args         = [ ./builder.sh ];
+
+  #src         = nxfs-sed-source;
+  src          = builtins.fetchTarball { url = "https://ftp.gnu.org/gnu/grep/grep-3.11.tar.xz"; };
+
+  target_tuple ="x86_64-pc-linux-gnu";
+}
