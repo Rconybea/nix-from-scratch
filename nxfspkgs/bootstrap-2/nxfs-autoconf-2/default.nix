ff1ed6a48b17375725f1808bf3c72592736ca82d
Roland Conybeare <rconybeare@gmail.com>
2024-12-26 00:55:09 -0500

    bootstrap stage 2: + nxfs-autoconf-2

--- nxfspkgs/bootstrap-2/nxfs-autoconf-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-autoconf-2/default.nix
@@ -7,11 +7,11 @@ let
   nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
   nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
   nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
+  nxfs-perl-2        = import ../nxfs-perl-2/default.nix;
+  nxfs-coreutils-2   = import ../nxfs-coreutils-2/default.nix;
 
   nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
   nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
-  nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
-  nxfs-perl-1        = import ../../bootstrap-1/nxfs-perl-1/default.nix;
 in
 
 derivation {
@@ -28,11 +28,11 @@ derivation {
   gnused       = nxfs-sed-2;
   gawk         = nxfs-gawk-2;
   grep         = nxfs-grep-2;
+  perl         = nxfs-perl-2;
 
   toolchain    = nxfs-toolchain-1;
   sysroot      = nxfs-sysroot-1;
-  coreutils    = nxfs-coreutils-1;
-  perl         = nxfs-perl-1;
+  coreutils    = nxfs-coreutils-2;
 
   builder      = "${nxfs-bash-2}/bin/bash";
   args         = [ ./builder.sh ];
