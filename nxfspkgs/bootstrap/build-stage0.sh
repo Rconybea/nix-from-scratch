a28b351eab4bb1745755721b112aa5b4d296fbad
Roland Conybeare <rconybeare@gmail.com>
2025-01-04 13:32:28 -0600

    stage0 build: automate build+adopt loop over stage0 packages.

---
+++ nxfspkgs/bootstrap/build-stage0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -x
+
+pushd nxfs-toolchain-0
+./copy2nix.sh
+nix-build
+popd
+
+pushd nxfs-sysroot-0
+./copy2nix.sh
+nix-build
+popd
+
+stage0pkgs="nxfs-patchelf-0 nxfs-gnumake-0 nxfs-coreutils-0 nxfs-bash-0 nxfs-tar-0 nxfs-sed-0 nxfs-grep-0 nxfs-gawk-0 nxfs-libxcrypt-0"
+
+for subdir in $stage0pkgs; do
+    pushd $subdir
+    (cd stage0 && make fetch && make config && make compile && make install)
+    ./copy2nix.sh
+    nix-build
+    popd
+done
---
+++ nxfspkgs/bootstrap/clean-stage0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this will clean local (outside-nix-store) builds.
+# 1. preserves tarballs from archive directory
+# 2. preserves toolchain+sysroot builds
+# 3. does not alter nix store state
+
+set -e
+set -x
+
+stage0pkgs="nxfs-patchelf-0 nxfs-gnumake-0 nxfs-coreutils-0 nxfs-bash-0 nxfs-tar-0 nxfs-sed-0 nxfs-grep-0 nxfs-gawk-0 nxfs-libxcrypt-0"
+
+for subdir in $stage0pkgs; do
+    pushd $subdir
+    (cd stage0 && make unpackclean)
+    popd
+done
