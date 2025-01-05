7308c4b47fa3a2d3b37bc127dc5ce65e5d92bb9a
Roland Conybeare <rconybeare@gmail.com>
2025-01-05 15:05:03 -0600

    bootstrap stage2: umbrella default.nix

---
+++ nxfspkgs/bootstrap-2/default.nix
@@ -0,0 +1,38 @@
+let
+  nxfs-gmp-2 = import ./nxfs-gmp-2/default.nix;
+  nxfs-m4-2 = import ./nxfs-m4-2/default.nix;
+  nxfs-patch-2 = import ./nxfs-patch-2/default.nix;
+  nxfs-perl-2 = import ./nxfs-perl-2/default.nix;
+  nxfs-findutils-2 = import ./nxfs-findutils-2/default.nix;
+  nxfs-coreutils-2 = import ./nxfs-coreutils-2/default.nix;
+  nxfs-bash-2 = import ./nxfs-bash-2/default.nix;
+  nxfs-tar-2 = import ./nxfs-tar-2/default.nix;
+  nxfs-gnumake-2 = import ./nxfs-gnumake-2/default.nix;
+  nxfs-gawk-2 = import ./nxfs-gawk-2/default.nix;
+  nxfs-grep-2 = import ./nxfs-grep-2/default.nix;
+  nxfs-sed-2 = import ./nxfs-sed-2/default.nix;
+
+  nxfs-bash-1 = import ../bootstrap-1/nxfs-bash-1/default.nix;
+
+  bash = "${nxfs-bash-1}/bin/bash";
+in
+
+derivation {
+  name = "nxfs-stage-2";
+  system = builtins.currentSystem;
+
+  builder = bash;
+
+  nxfs-gmp-2 = nxfs-gmp-2;
+  nxfs-m4-2 = nxfs-m4-2;
+  nxfs-patch-2 = nxfs-patch-2;
+  nxfs-perl-2 = nxfs-perl-2;
+  nxfs-findutils-2 = nxfs-findutils-2;
+  nxfs-coreutils-2 = nxfs-coreutils-2;
+  nxfs-bash-2 = nxfs-bash-2;
+  nxfs-tar-2 = nxfs-tar-2;
+  nxfs-gnumake-2 = nxfs-gnumake-2;
+  nxfs-gawk-2 = nxfs-gawk-2;
+  nxfs-grep-2 = nxfs-grep-2;
+  nxfs-sed-2 = nxfs-sed-2;
+}
