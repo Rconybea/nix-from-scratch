799038d1d01120fdbb6f74b59da91ea055b9cf16
Roland Conybeare <rconybeare@gmail.com>
2025-01-05 16:04:57 -0600

    bootstrap stage2: + nxfs-mpc-2

---
+++ nxfspkgs/bootstrap-2/nxfs-mpc-2/default.nix
@@ -0,0 +1,48 @@
+let
+  nxfs-mpfr-2        = import ../nxfs-mpfr-2/default.nix;
+  nxfs-gmp-2         = import ../nxfs-gmp-2/default.nix;
+  nxfs-m4-2          = import ../nxfs-m4-2/default.nix;
+  nxfs-gcc-wrapper-2 = import ../nxfs-gcc-wrapper-2/default.nix;
+  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
+  nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
+  nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
+  nxfs-gnumake-2     = import ../nxfs-gnumake-2/default.nix;
+  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
+  nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
+  nxfs-coreutils-2   = import ../nxfs-coreutils-2/default.nix;
+
+  nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
+  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
+
+in
+
+derivation {
+  name         = "nxfs-mpc-2";
+
+  system       = builtins.currentSystem;
+
+  toolchain    = nxfs-toolchain-1;
+  sysroot      = nxfs-sysroot-1;
+
+  mpfr         = nxfs-mpfr-2;
+  gmp          = nxfs-gmp-2;
+  m4           = nxfs-m4-2;
+  coreutils    = nxfs-coreutils-2;
+  bash         = nxfs-bash-2;
+  tar          = nxfs-tar-2;
+  gnumake      = nxfs-gnumake-2;
+  gawk         = nxfs-gawk-2;
+  sed          = nxfs-sed-2;
+  grep         = nxfs-grep-2;
+  gcc_wrapper  = nxfs-gcc-wrapper-2;
+
+  builder      = "${nxfs-bash-2}/bin/bash";
+  args         = [ ./builder.sh ];
+
+  src          = builtins.fetchTarball { name = "mpc-1.3.1-source";
+                                         url = "https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz";
+                                         sha256 = "1b6layaybj039fajx8dpy2zvcfy7s02y3y4lficz16vac0fsd0jk";
+                                       };
+
+  target_tuple ="x86_64-pc-linux-gnu";
+}
