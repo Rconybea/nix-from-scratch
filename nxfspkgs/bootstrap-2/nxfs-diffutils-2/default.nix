e359048dc0ce296bbd35fa5318894b0d63fe0c66
Roland Conybeare <rconybeare@gmail.com>
2025-01-19 13:59:17 -0500

    bootstrap stage 2: nxfs-diffutils-2: refactor to make diffutils avail earlier

--- nxfspkgs/bootstrap-2/nxfs-diffutils-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-diffutils-2/default.nix
@@ -1,13 +1,14 @@
 let
 
   nxfs-gcc-wrapper-2 = import ../nxfs-gcc-wrapper-2/default.nix;
-  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
-  nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
-  nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
-  nxfs-gnumake-2     = import ../nxfs-gnumake-2/default.nix;
-  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
-  nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
-  nxfs-coreutils-2   = import ../nxfs-coreutils-2/default.nix;
+
+  nxfs-sed-1         = import ../../bootstrap-1/nxfs-sed-1/default.nix;
+  nxfs-grep-1        = import ../../bootstrap-1/nxfs-grep-1/default.nix;
+  nxfs-gawk-1        = import ../../bootstrap-1/nxfs-gawk-1/default.nix;
+  nxfs-gnumake-1     = import ../../bootstrap-1/nxfs-gnumake-1/default.nix;
+  nxfs-tar-1         = import ../../bootstrap-1/nxfs-tar-1/default.nix;
+  nxfs-bash-1        = import ../../bootstrap-1/nxfs-bash-1/default.nix;
+  nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
 
   nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
   nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
@@ -22,16 +23,16 @@ derivation {
   toolchain    = nxfs-toolchain-1;
   sysroot      = nxfs-sysroot-1;
 
-  coreutils    = nxfs-coreutils-2;
-  bash         = nxfs-bash-2;
-  tar          = nxfs-tar-2;
-  gnumake      = nxfs-gnumake-2;
-  gawk         = nxfs-gawk-2;
-  sed          = nxfs-sed-2;
-  grep         = nxfs-grep-2;
+  coreutils    = nxfs-coreutils-1;
+  bash         = nxfs-bash-1;
+  tar          = nxfs-tar-1;
+  gnumake      = nxfs-gnumake-1;
+  gawk         = nxfs-gawk-1;
+  sed          = nxfs-sed-1;
+  grep         = nxfs-grep-1;
   gcc_wrapper  = nxfs-gcc-wrapper-2;
 
-  builder      = "${nxfs-bash-2}/bin/bash";
+  builder      = "${nxfs-bash-1}/bin/bash";
   args         = [ ./builder.sh ];
 
   src          = builtins.fetchTarball { name = "diffutils-3.10-source";
