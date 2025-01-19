e6922039c66327c05cf846b33a220f78069f97f4
Roland Conybeare <rconybeare@gmail.com>
2025-01-19 13:47:44 -0500

    bootstrap stage 0: update build/clean phase scripts

--- nxfspkgs/bootstrap/build-stage0.sh
+++ nxfspkgs/bootstrap/build-stage0.sh
@@ -13,7 +13,7 @@ pushd nxfs-sysroot-0
 nix-build
 popd
 
-stage0pkgs="nxfs-patchelf-0 nxfs-gnumake-0 nxfs-coreutils-0 nxfs-bash-0 nxfs-tar-0 nxfs-sed-0 nxfs-grep-0 nxfs-gawk-0 nxfs-libxcrypt-0"
+stage0pkgs="nxfs-patchelf-0 nxfs-gnumake-0 nxfs-coreutils-0 nxfs-bash-0 nxfs-tar-0 nxfs-sed-0 nxfs-grep-0 nxfs-gawk-0 nxfs-libxcrypt-0 nxfs-findutils-0 nxfs-diffutils-0"
 
 for subdir in $stage0pkgs; do
     pushd $subdir
--- nxfspkgs/bootstrap/clean-stage0.sh
+++ nxfspkgs/bootstrap/clean-stage0.sh
@@ -8,10 +8,10 @@
 set -e
 set -x
 
-stage0pkgs="nxfs-patchelf-0 nxfs-gnumake-0 nxfs-coreutils-0 nxfs-bash-0 nxfs-tar-0 nxfs-sed-0 nxfs-grep-0 nxfs-gawk-0 nxfs-libxcrypt-0"
+stage0pkgs="nxfs-patchelf-0 nxfs-gnumake-0 nxfs-coreutils-0 nxfs-bash-0 nxfs-tar-0 nxfs-sed-0 nxfs-grep-0 nxfs-gawk-0 nxfs-libxcrypt-0 nxfs-findutils-0 nxfs-diffutils-0"
 
 for subdir in $stage0pkgs; do
     pushd $subdir
-    (cd stage0 && make unpackclean)
+    (cd stage0 && rm -rf build && make unpackclean)
     popd
 done
