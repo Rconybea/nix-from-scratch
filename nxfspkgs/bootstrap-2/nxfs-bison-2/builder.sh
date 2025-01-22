9c1cf48e87b8fe2b5434ee8dc813c32f5dec3008
Roland Conybeare <rconybeare@gmail.com>
2025-01-22 00:01:09 -0500

    bootstrap stage 21: nxfs-bison-2: + diffutils + findutils

--- nxfspkgs/bootstrap-2/nxfs-bison-2/builder.sh
+++ nxfspkgs/bootstrap-2/nxfs-bison-2/builder.sh
@@ -3,7 +3,6 @@
 set -e
 set -x
 
-echo "gcc_wrapper=${gcc_wrapper}"
 echo "toolchain=${toolchain}"
 echo "flex=${flex}"
 echo "gnumake=${gnumake}"
@@ -17,10 +16,9 @@ echo "diffutils=${diffutils}"
 echo "findutils=${findutils}"
 echo "coreutils=${coreutils}"
 echo "sysroot=${sysroot}"
-#echo "mkdir=${mkdir}"
-#echo "head=${head}"
 echo "bash=${bash}"
 echo "src=${src}"
+echo "gcc_wrapper=${gcc_wrapper}"
 echo "target_tuple=${target_tuple}"
 echo "TMPDIR=${TMPDIR}"
 
@@ -30,7 +28,7 @@ echo "TMPDIR=${TMPDIR}"
 # 3. ${toolchain}/bin                     has x86_64-pc-linux-gnu-ar
 # 4. ${toolchain}/x86_64-pc-linux-gnu/bin has ar  <- autotools looks for this
 #
-export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${flex}/bin:${texinfo}/bin:${m4}/bin:${diffutils}/bin:${findutils}/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${bash}/bin"
+export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${flex}/bin:${texinfo}/bin:${m4}/bin:${coreutils}/bin:${bash}/bin:${tar}/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${diffutils}/bin:${findutils}/bin"
 
 ls -l ${toolchain}/x86_64-pc-linux-gnu/bin
 
--- nxfspkgs/bootstrap-2/nxfs-bison-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-bison-2/default.nix
@@ -1,18 +1,18 @@
 let
 
   nxfs-flex-2        = import ../nxfs-flex-2/default.nix;
-  nxfs-gcc-wrapper-2 = import ../nxfs-gcc-wrapper-2/default.nix;
-  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
-  nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
-  nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
-  nxfs-gnumake-2     = import ../nxfs-gnumake-2/default.nix;
-  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
+#  nxfs-texinfo-2     = import ../nxfs-texinfo-2/default.nix;
+  nxfs-m4-2          = import ../nxfs-m4-2/default.nix;
+  nxfs-coreutils-2   = import ../nxfs-coreutils-2/default.nix;
   nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
-  nxfs-texinfo-2     = import ../nxfs-texinfo-2/default.nix;
+  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
+  nxfs-gnumake-2     = import ../nxfs-gnumake-2/default.nix;
+  nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
+  nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
+  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
   nxfs-diffutils-2   = import ../nxfs-diffutils-2/default.nix;
   nxfs-findutils-2   = import ../nxfs-findutils-2/default.nix;
-  nxfs-coreutils-2   = import ../nxfs-coreutils-2/default.nix;
-  nxfs-m4-2          = import ../nxfs-m4-2/default.nix;
+  nxfs-gcc-wrapper-2 = import ../nxfs-gcc-wrapper-2/default.nix;
 
   nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
   nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
@@ -28,10 +28,8 @@ derivation {
   sysroot      = nxfs-sysroot-1;
 
   flex         = nxfs-flex-2;
-  texinfo      = nxfs-texinfo-2;
+#  texinfo      = nxfs-texinfo-2;
   m4           = nxfs-m4-2;
-  diffutils    = nxfs-diffutils-2;
-  findutils    = nxfs-findutils-2;
   coreutils    = nxfs-coreutils-2;
   bash         = nxfs-bash-2;
   tar          = nxfs-tar-2;
@@ -39,6 +37,8 @@ derivation {
   gawk         = nxfs-gawk-2;
   sed          = nxfs-sed-2;
   grep         = nxfs-grep-2;
+  findutils    = nxfs-findutils-2;
+  diffutils    = nxfs-diffutils-2;
   gcc_wrapper  = nxfs-gcc-wrapper-2;
 
   builder      = "${nxfs-bash-2}/bin/bash";
