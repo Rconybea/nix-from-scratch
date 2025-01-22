aea65321f0f788a29a236fa9b71aa5bbda46b8b8
Roland Conybeare <rconybeare@gmail.com>
2025-01-21 23:57:26 -0500

    bootstrap stage 2: nxfs-flex-2: + findutils + diffutils + file deps

--- nxfspkgs/bootstrap-2/nxfs-flex-2/builder.sh
+++ nxfspkgs/bootstrap-2/nxfs-flex-2/builder.sh
@@ -3,22 +3,22 @@
 set -e
 set -x
 
-echo "gcc_wrapper=${gcc_wrapper}"
 echo "toolchain=${toolchain}"
+echo "texinfo=${texinfo}"
+echo "m4=${m4}"
+echo "coreutils=${coreutils}"
+echo "bash=${bash}"
+echo "tar=${tar}"
 echo "gnumake=${gnumake}"
 echo "gawk=${gawk}"
 echo "grep=${grep}"
 echo "sed=${sed}"
-echo "tar=${tar}"
-echo "texinfo=${texinfo}"
-echo "m4=${m4}"
 echo "diffutils=${diffutils}"
 echo "findutils=${findutils}"
-echo "coreutils=${coreutils}"
+echo "gcc_wrapper=${gcc_wrapper}"
 echo "sysroot=${sysroot}"
 #echo "mkdir=${mkdir}"
 #echo "head=${head}"
-echo "bash=${bash}"
 echo "src=${src}"
 echo "target_tuple=${target_tuple}"
 echo "TMPDIR=${TMPDIR}"
@@ -29,7 +29,7 @@ echo "TMPDIR=${TMPDIR}"
 # 3. ${toolchain}/bin                     has x86_64-pc-linux-gnu-ar
 # 4. ${toolchain}/x86_64-pc-linux-gnu/bin has ar  <- autotools looks for this
 #
-export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${texinfo}/bin:${m4}/bin:${diffutils}/bin:${findutils}/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${bash}/bin"
+export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${file}/bin:${texinfo}/bin:${m4}/bin:${diffutils}/bin:${findutils}/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${findutils}/bin:${diffutils}/bin:${bash}/bin"
 
 ls -l ${toolchain}/x86_64-pc-linux-gnu/bin
 
