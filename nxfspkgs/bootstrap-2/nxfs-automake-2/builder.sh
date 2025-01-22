1c81825f22f80c5ac393f5afcf1dd284baa37082
Roland Conybeare <rconybeare@gmail.com>
2025-01-21 23:56:45 -0500

    bootstrap stage 2: nxfs-automake-2: + diffutils + findutils deps

--- nxfspkgs/bootstrap-2/nxfs-automake-2/builder.sh
+++ nxfspkgs/bootstrap-2/nxfs-automake-2/builder.sh
@@ -30,11 +30,12 @@ set -x
 # 3. ${toolchain}/bin                     has x86_64-pc-linux-gnu-ar
 # 4. ${toolchain}/x86_64-pc-linux-gnu/bin has ar  <- autotools looks for this
 #
-export PATH="${findutils}/bin:${autoconf}/bin:${perl}/bin:${m4}/bin:${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${bash}/bin"
+export PATH="${autoconf}/bin:${perl}/bin:${m4}/bin:${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${findutils}/bin:${diffutils}/bin:${bash}/bin"
 
 #src2=${src}
 src2=${TMPDIR}/src2
 builddir=${TMPDIR}/build
+#builddir=${src2}
 
 mkdir -p ${src2}
 mkdir -p ${builddir}
@@ -42,19 +43,42 @@ mkdir -p ${builddir}
 mkdir ${out}
 
 bash_program=${bash}/bin/bash
+perl_program=${perl}/bin/perl
+
+# NOTE: problem encountered in nix build:
+#
+# 1. nix build triggers make rule to regenerate src/t/testsuite-part.am
+# 2. make rule lookes buggy afacit:  it assumes can directory $(pwd)/t exists,
+#    to hold .tmp file
+#    (note that if you attempt in-source-tree build, this requirement probably
+#    gets satisfied accidentally
+#
+#    $(srcdir)/t/testsuite-part.am:
+#      $(AM_V_at)rm -f t/testsuite-part.tmp $@
+#      echo "hi roly: pwd [$$(pwd)]"
+#      /usr/bin/strace -f $(AM_V_GEN)$(PERL) $(srcdir)/gen-testsuite-part \
+#        --srcdir $(srcdir) > t/testsuite-part.tmp
+#      $(AM_V_at)chmod a-w t/testsuite-part.tmp
+#      $(AM_V_at)mv -f t/testsuite-part.tmp $@
+#
+# 3. try to finesse by creating the directory..
+
+mkdir -p ${builddir}/t
 
 # 1. copy source tree to temporary directory,
 #
 (cd ${src} && (tar cf - . | tar xf - -C ${src2}))
 
-# 2. substitute nix-store path-to-bash for /bin/sh.
-#
-#
-#chmod -R +w ${src2}
+chmod -R +w ${src2}
 
-#(cd ${src2} && ${bash_program} ${m4_patch})
-
-#chmod -R -w ${src2}
+# patching shebangs seems to make everything go sideways.  nixpkgs does not either, to allow invoking nix-owned automake from outside nix
+#
+#(cd ${src2} &&  find . -type f | grep -v '*\.am$' | xargs --replace=xx sed -i -e 's:#! */bin/sh:#! '${bash_program}':' -e 's:#! */usr/bin/env perl:#! '${perl_program}':' xx)
+#(cd ${src2} && sed -i -e 's:/bin/sh:'${bash_program}':' GNUmakefile)
+#(cd ${src2} && sed -i -e 's:/bin/sh:'${bash_program}':' bootstrap)
+#(cd ${src2} && sed -i -e 's:/bin/sh:'${bash_program}':' lib/config.guess)
+#(cd ${src2} && sed -i -e 's:/bin/sh:'${bash_program}':' t/ax/am-test-lib.sh)
+#(cd ${src2} && sed -i -e 's:/bin/sh:'${bash_program}':' t/missing3.sh)
 
 # Must skip:
 #   .m4 and .in files (assume they trigger re-running autoconf)
@@ -62,7 +86,7 @@ bash_program=${bash}/bin/bash
 #
 #sed -i -e "s:/bin/sh:${bash_program}:g" ${src2}/configure #${src2}/build-aux/*
 
-# ${src}/configure honors CONFIG_SHELL
+# ${src2}/configure honors CONFIG_SHELL
 export CONFIG_SHELL="${bash_program}"
 
 # 1.
@@ -73,8 +97,17 @@ export CONFIG_SHELL="${bash_program}"
 # do need to give --host and --build arguments to configure,
 # since we're using a cross compiler.
 
-(cd ${builddir} && ${bash_program} ${src2}/configure --prefix=${out} --host=${target_tuple} --build=${target_tuple} CFLAGS="-I${sysroot}/usr/include" LDFLAGS="-Wl,-enable-new-dtags")
+(cd ${builddir} && bash ${src2}/configure --prefix=${out} --host=${target_tuple} --build=${target_tuple} CC="nxfs-gcc" CXX="nxfs-g++" CFLAGS="-I${sysroot}/usr/include" LDFLAGS="-Wl,-enable-new-dtags")
 
+(cd ${builddir} && sed -i -e 's:#! */bin/sh:#! '${bash_program}':' ./pre-inst-env)
+
+# debris from investigating build failure
+#(cd ${builddir} && sed -i -e '/\$(AM_V_GEN)\$(PERL) \$(srcdir)\/gen-testsuite-part/i placeholder$(AM_V_GEN)$(PERL) -Dtrace $(srcdir)/gen-testsuite-part --srcdir $(srcdir)' Makefile)
+#(cd ${builddir} && sed -i -e '/\$(AM_V_GEN)\$(PERL) \$(srcdir)\/gen-testsuite-part/i placeholderecho hiroly [$$(pwd)]' Makefile)
+#(cd ${builddir} && sed -i -e 's:^placeholder:\t:' Makefile)
+#(cd ${builddir} && grep -C 5 '/gen-testsuite-part' Makefile)
+
+#(cd ${builddir} && /usr/bin/strace -f -e trace=chdir make SHELL=${CONFIG_SHELL})
 (cd ${builddir} && make SHELL=${CONFIG_SHELL})
 
 (cd ${builddir} && make install SHELL=${CONFIG_SHELL})
