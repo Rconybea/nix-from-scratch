671fe82ce32547398ca8c3691ee85b1ad4b5d428
Roland Conybeare <rconybeare@gmail.com>
2025-01-26 13:59:54 -0500

    bootstrap stage 2: + nxfs-gzip-2 (needed by glibc)

---
+++ nxfspkgs/bootstrap-2/nxfs-gzip-2/default.nix
@@ -0,0 +1,53 @@
+let
+
+  #nxfs-texinfo-2     = import ../nxfs-texinfo-2/default.nix;
+  #nxfs-m4-2          = import ../nxfs-m4-2/default.nix;
+
+  nxfs-coreutils-2   = import ../nxfs-coreutils-2/default.nix;
+  nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
+  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
+  nxfs-gnumake-2     = import ../nxfs-gnumake-2/default.nix;
+  nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
+  nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
+  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
+  nxfs-findutils-2   = import ../nxfs-findutils-2/default.nix;
+  nxfs-diffutils-2   = import ../nxfs-diffutils-2/default.nix;
+  nxfs-gcc-wrapper-2 = import ../nxfs-gcc-wrapper-2/default.nix;
+
+  nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
+  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
+
+in
+
+derivation {
+  name         = "nxfs-gzip-2";
+
+  system       = builtins.currentSystem;
+
+  toolchain    = nxfs-toolchain-1;
+  sysroot      = nxfs-sysroot-1;
+
+  #texinfo      = nxfs-texinfo-2;
+  #m4           = nxfs-m4-2;
+  coreutils    = nxfs-coreutils-2;
+  bash         = nxfs-bash-2;
+  tar          = nxfs-tar-2;
+  gnumake      = nxfs-gnumake-2;
+  gawk         = nxfs-gawk-2;
+  grep         = nxfs-grep-2;
+  sed          = nxfs-sed-2;
+  findutils    = nxfs-findutils-2;
+  diffutils    = nxfs-diffutils-2;
+  gcc_wrapper  = nxfs-gcc-wrapper-2;
+
+  builder      = "${nxfs-bash-2}/bin/bash";
+  args         = [ ./builder.sh ];
+
+  src          = builtins.fetchTarball { name = "gzip-1.13-source";
+                                         url = "https://ftp.gnu.org/gnu/gzip/gzip-1.13.tar.xz";
+                                         sha256 = "093w3a12220gzy00qi9zy52mhjlgyyh7kiimsz5xa00fgf81rbp9"; };
+
+  outputs      = [ "out" "source" ];
+
+  target_tuple ="x86_64-pc-linux-gnu";
+}
