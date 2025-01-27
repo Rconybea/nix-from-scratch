5b19b766acd7667d3f06abd7d82240c4e3bf05e4
Roland Conybeare <rconybeare@gmail.com>
2025-01-27 08:02:30 -0600

    bootstrap stage 2: + nxfs-patchelf-2

---
+++ nxfspkgs/bootstrap-2/nxfs-patchelf-2/default.nix
@@ -0,0 +1,45 @@
+let
+  nxfs-coreutils-2   = import ../nxfs-coreutils-2/default.nix;
+  nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
+  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
+  nxfs-gnumake-2     = import ../nxfs-gnumake-2/default.nix;
+  nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
+  nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
+  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
+  nxfs-findutils-2   = import ../nxfs-findutils-2/default.nix;
+  nxfs-diffutils-2   = import ../nxfs-diffutils-2/default.nix;
+  nxfs-gcc-wrapper-2 = import ../nxfs-gcc-wrapper-2/default.nix;
+
+  nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
+  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
+in
+
+derivation {
+  name         = "nxfs-patch-2";
+
+  system       = builtins.currentSystem;
+
+  coreutils    = nxfs-coreutils-2;
+  bash         = nxfs-bash-2;
+  tar          = nxfs-tar-2;
+  gnumake      = nxfs-gnumake-2;
+  gawk         = nxfs-gawk-2;
+  grep         = nxfs-grep-2;
+  gnused       = nxfs-sed-2;
+  sed          = nxfs-sed-2;
+  findutils    = nxfs-findutils-2;
+  diffutils    = nxfs-diffutils-2;
+  gcc_wrapper  = nxfs-gcc-wrapper-2;
+
+  toolchain    = nxfs-toolchain-1;
+  sysroot      = nxfs-sysroot-1;
+
+  builder      = "${nxfs-bash-2}/bin/bash";
+  args         = [ ./builder.sh ];
+
+  src          = builtins.fetchTarball { name = "patchelf-0.18.0-source";
+                                         url = "https://github.com/NixOS/patchelf/releases/download/0.18.0/patchelf-0.18.0.tar.gz";
+                                         sha256 = "0s328cmgrbhsc344q323dhg70h8lf8532ywjf8jwjirxq6a5h06w"; };
+
+  target_tuple ="x86_64-pc-linux-gnu";
+}
