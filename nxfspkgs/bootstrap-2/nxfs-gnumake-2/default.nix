55de0a796b7af32c5edd9fd37a7582b1208ffca4
Roland Conybeare <rconybeare@gmail.com>
2025-03-02 00:17:29 -0600

    bootstrap stage 2: nxfs-gnumake-2: .nix style nits

--- nxfspkgs/bootstrap-2/nxfs-gnumake-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-gnumake-2/default.nix
@@ -1,18 +1,19 @@
 let
   nxfs-gnumake-1     = import ../../bootstrap-1/nxfs-gnumake-1/default.nix;
 
+  nxfs-toolchain-wrapper-1 = import ../../bootstrap-1/nxfs-toolchain-wrapper-1/default.nix;
+
+  nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
+  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
+  nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
+
   nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
+  nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
+  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
   nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
   nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
   nxfs-findutils-2   = import ../nxfs-findutils-2/default.nix;
   nxfs-diffutils-2   = import ../nxfs-diffutils-2/default.nix;
-  nxfs-toolchain-wrapper-1 = import ../../bootstrap-1/nxfs-toolchain-wrapper-1/default.nix;
-
-  nxfs-tar-1         = import ../../bootstrap-1/nxfs-tar-1/default.nix;
-  nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
-  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
-  nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
-  nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
 
   nxfs-defs = import ../nxfs-defs.nix;
 in
@@ -23,13 +24,13 @@ derivation {
   system       = builtins.currentSystem;
 
   gnumake      = nxfs-gnumake-1;
-  bash         = nxfs-bash-2;
   toolchain    = nxfs-toolchain-1;
   sysroot      = nxfs-sysroot-1;
   coreutils    = nxfs-coreutils-1;
-  tar          = nxfs-tar-1;
 
   gawk         = nxfs-gawk-2;
+  bash         = nxfs-bash-2;
+  tar          = nxfs-tar-2;
   grep         = nxfs-grep-2;
   sed          = nxfs-sed-2;
   findutils    = nxfs-findutils-2;
