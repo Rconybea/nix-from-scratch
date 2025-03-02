7c113c8973b9fbbbce52e44300e5c01b3b56c9b2
Roland Conybeare <rconybeare@gmail.com>
2025-03-02 00:18:02 -0600

    bootstrap stage 2: nxfs-coreutils-2: .nix style nits

--- nxfspkgs/bootstrap-2/nxfs-coreutils-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-coreutils-2/default.nix
@@ -1,12 +1,12 @@
 let
   nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
 
-  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
-  nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
-  nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
   nxfs-gnumake-2     = import ../nxfs-gnumake-2/default.nix;
-  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
+  nxfs-gawk-2        = import ../nxfs-gawk-2/default.nix;
   nxfs-bash-2        = import ../nxfs-bash-2/default.nix;
+  nxfs-tar-2         = import ../nxfs-tar-2/default.nix;
+  nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
+  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
   nxfs-findutils-2   = import ../nxfs-findutils-2/default.nix;
   nxfs-diffutils-2   = import ../nxfs-diffutils-2/default.nix;
   nxfs-toolchain-wrapper-1 = import ../../bootstrap-1/nxfs-toolchain-wrapper-1/default.nix;
@@ -26,12 +26,12 @@ derivation {
   sysroot      = nxfs-sysroot-1;
   coreutils    = nxfs-coreutils-1;
 
-  bash         = nxfs-bash-2;
-  tar          = nxfs-tar-2;
   gnumake      = nxfs-gnumake-2;
   gawk         = nxfs-gawk-2;
-  sed          = nxfs-sed-2;
+  bash         = nxfs-bash-2;
+  tar          = nxfs-tar-2;
   grep         = nxfs-grep-2;
+  sed          = nxfs-sed-2;
   findutils    = nxfs-findutils-2;
   diffutils    = nxfs-diffutils-2;
   gcc_wrapper  = nxfs-toolchain-wrapper-1;
