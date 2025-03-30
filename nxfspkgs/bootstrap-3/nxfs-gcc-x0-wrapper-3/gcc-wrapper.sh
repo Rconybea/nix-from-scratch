9bac3924507c2796a1bb3c911f7209f80fbdfefc
Roland Conybeare <rconybeare@gmail.com>
2025-03-30 14:05:30 -0500

    bootstrap-3: also put -B in nxfs-gcc

--- nxfspkgs/bootstrap-3/nxfs-gcc-x0-wrapper-3/gcc-wrapper.sh
+++ nxfspkgs/bootstrap-3/nxfs-gcc-x0-wrapper-3/gcc-wrapper.sh
@@ -31,5 +31,5 @@ if [[ $# -eq 1 ]] && [[ "$1" == '-v' ]]; then
     #
     ${unwrapped_gcc} -v
 else
-    ${unwrapped_gcc} -Wl,-rpath=${NXFS_SYSROOT_DIR}/lib -Wl,-dynamic-linker=${NXFS_SYSROOT_DIR}/lib/ld-linux-x86-64.so.2 "${@}"
+    ${unwrapped_gcc} -B${NXFS_SYSROOT_DIR}/lib -Wl,-rpath=${NXFS_SYSROOT_DIR}/lib -Wl,-dynamic-linker=${NXFS_SYSROOT_DIR}/lib/ld-linux-x86-64.so.2 "${@}"
 fi
