4d8bff42eb2357474902d47df990d3c56a01a7ea
Roland Conybeare <rconybeare@gmail.com>
2025-10-12 00:45:49 -0400

    nxfs-sed-2: direct toolchain + -euo pipefail + streamlining

--- nxfspkgs/bootstrap-2/nxfs-sed-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-sed-2/default.nix
@@ -3,13 +3,12 @@ let
 
   nxfs-gnumake-1     = import ../../bootstrap-1/nxfs-gnumake-1/default.nix;
   nxfs-gawk-1        = import ../../bootstrap-1/nxfs-gawk-1/default.nix;
-  nxfs-tar-1         = import ../../bootstrap-1/nxfs-tar-1/default.nix;
+  nxfs-gnutar-1      = import ../../bootstrap-1/nxfs-tar-1/default.nix;
   nxfs-grep-1        = import ../../bootstrap-1/nxfs-grep-1/default.nix;
   nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
-  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
   nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
   nxfs-bash-1        = import ../../bootstrap-1/nxfs-bash-1/default.nix;
-  nxfs-sed-1         = import ../../bootstrap-1/nxfs-sed-1/default.nix;
+  nxfs-gnused-1      = import ../../bootstrap-1/nxfs-sed-1/default.nix;
 
   nxfs-findutils-2   = import ../nxfs-findutils-2/default.nix;
   nxfs-diffutils-2   = import ../nxfs-diffutils-2/default.nix;
@@ -25,13 +24,11 @@ derivation {
   bash         = nxfs-bash-1;
   gcc_wrapper  = nxfs-toolchain-wrapper-1;
   toolchain    = nxfs-toolchain-1;
-  sysroot      = nxfs-sysroot-1;
   coreutils    = nxfs-coreutils-1;
-  tar          = nxfs-tar-1;
-  gnused       = nxfs-sed-1;
+  gnutar       = nxfs-gnutar-1;
+  gnused       = nxfs-gnused-1;
   gawk         = nxfs-gawk-1;
   grep         = nxfs-grep-1;
-  sed          = nxfs-sed-1;
   findutils    = nxfs-findutils-2;
   diffutils    = nxfs-diffutils-2;
 
@@ -39,8 +36,6 @@ derivation {
   args         = [ ./builder.sh ];
 
   src          = builtins.fetchTarball { name = "sed-4.9-source";
-                                         url = "https://ftp.gnu.org/gnu/sed/sed-4.9.tar.xz";
+                                         url = "https://ftpmirror.gnu.org/gnu/sed/sed-4.9.tar.xz";
                                          sha256 = "170m9hyxnhnxisvmii5z7m8i446ab97kam10rqjylj70dk8wh169"; };
-
-  target_tuple = nxfs-defs.target_tuple;
 }
