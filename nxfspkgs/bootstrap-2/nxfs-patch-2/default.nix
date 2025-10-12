26d45bc8f8abeaf0f09a2aa944ef49f760e36bde
Roland Conybeare <rconybeare@gmail.com>
2025-10-12 01:16:16 -0400

    nxfs-patch-2: -euo pipefail + direct toolchain + streamline

--- nxfspkgs/bootstrap-2/nxfs-patch-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-patch-2/default.nix
@@ -11,7 +11,6 @@ let
   nxfs-toolchain-wrapper-1 = import ../../bootstrap-1/nxfs-toolchain-wrapper-1/default.nix;
 
   nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
-  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
 
   nxfs-defs = import ../nxfs-defs.nix;
 in
@@ -34,14 +33,11 @@ derivation {
   gcc_wrapper  = nxfs-toolchain-wrapper-1;
 
   toolchain    = nxfs-toolchain-1;
-  sysroot      = nxfs-sysroot-1;
 
   builder      = "${nxfs-bash-2}/bin/bash";
   args         = [ ./builder.sh ];
 
   src          = builtins.fetchTarball { name = "patch-2.7.6-source";
-                                         url = "https://ftp.gnu.org/gnu/patch/patch-2.7.6.tar.xz";
+                                         url = "https://ftpmirror.gnu.org/gnu/patch/patch-2.7.6.tar.xz";
                                          sha256 = "1yiy0xq1ha193yga0canc9ijw4hbd92c93l7ksqlhmzsn2yph39n"; };
-
-  target_tuple = nxfs-defs.target_tuple;
 }
