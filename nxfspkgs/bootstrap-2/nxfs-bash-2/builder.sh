96d233741ffbe93377409e3ad647082782013377
Roland Conybeare <rconybeare@gmail.com>
2025-10-12 00:48:39 -0400

    nxfs-bash-2: direct toolchain + streamlining + set -euo pipefail

--- nxfspkgs/bootstrap-2/nxfs-bash-2/builder.sh
+++ nxfspkgs/bootstrap-2/nxfs-bash-2/builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 set -x
 
 echo "gcc_wrapper=${gcc_wrapper}"
@@ -13,39 +13,20 @@ echo "tar=${tar}"
 echo "coreutils=${coreutils}"
 echo "findutils=${findutils}"
 echo "diffutils=${diffutils}"
-echo "sysroot=${sysroot}"
 echo "bash=${bash}"
 echo "ncurses=${ncurses}"
 echo "src=${src}"
-echo "target_tuple=${target_tuple}"
 echo "TMPDIR=${TMPDIR}"
 
-# 1. ${gcc_wrapper}/bin/x86_64-pc-linux-gnu-{gcc,g++} builds viable executables.
-# 2. ${toolchain}/bin/x86_64-pc-linux-gnu-gcc can build executables,
-#    but they won't run unless we pass special linker flags
-# 3. ${toolchain}/bin                     has x86_64-pc-linux-gnu-ar
-# 4. ${toolchain}/x86_64-pc-linux-gnu/bin has ar  <- autotools looks for this
-#
-export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${ncurses}/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${findutils}/bin:${diffutils}/bin:${bash}/bin"
+export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${ncurses}/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${findutils}/bin:${diffutils}/bin:${bash}/bin"
 
 ls -l ${toolchain}/x86_64-pc-linux-gnu/bin
 
-src2=${TMPDIR}/src2
 builddir=${TMPDIR}/build
 
-mkdir -p ${src2}
 mkdir -p ${builddir}
 
 mkdir ${out}
-
-# 1. copy source tree to temporary directory,
-#
-(cd ${src} && (tar cf - . | tar xf - -C ${src2}))
-
-# 2. substitute nix-store path-to-bash for /bin/sh.
-#
-#
-#chmod -R +w ${src2}
 
 bash_program=${bash}/bin/bash
 # Must skip:
@@ -53,23 +34,13 @@ bash_program=${bash}/bin/bash
 #   test/ files
 #
 
-#sed -i -e "s:/bin/sh:${bash_program}:g" ${src2}/configure #${src2}/build-aux/*
-
 # ${src}/configure honors CONFIG_SHELL
 export CONFIG_SHELL="${bash_program}"
 
-# 1.
-# we shouldn't need special compiler/linker instructions,
-# since stage-1 toolchain "knows where it lives"
-#
-# 2.
-# do need to give --host and --build arguments to configure,
-# since we're using a cross compiler.
-
-CFLAGS="-I${ncurses}/include -I${sysroot}/usr/include"
+CFLAGS="-I${ncurses}/include -I${toolchain}/include"
 LDFLAGS="-L${ncurses}/lib -Wl,-rpath,${ncurses}/lib -Wl,-enable-new-dtags"
 
-(cd ${builddir} && ${bash_program} ${src2}/configure --prefix=${out} --host=${target_tuple} --build=${target_tuple} --with-curses --without-bash-malloc bash_cv_strtold_broken=no CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS")
+(cd ${builddir} && ${bash_program} ${src}/configure --prefix=${out} --with-curses --without-bash-malloc bash_cv_strtold_broken=no CPP=cpp CPPFLAGS="-I${toolchain}/include" CC="nxfs-gcc" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS")
 
 (cd ${builddir} && make SHELL=${CONFIG_SHELL})
 
@@ -77,3 +48,5 @@ LDFLAGS="-L${ncurses}/lib -Wl,-rpath,${ncurses}/lib -Wl,-enable-new-dtags"
 
 # post-install
 (cd ${out}/bin && ln -sfv bash sh)
+
+${out}/bin/bash --version
--- nxfspkgs/bootstrap-2/nxfs-bash-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-bash-2/default.nix
@@ -12,7 +12,6 @@ let
   nxfs-toolchain-wrapper-1 = import ../../bootstrap-1/nxfs-toolchain-wrapper-1/default.nix;
 
   nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
-  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
   nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
 
   nxfs-defs = import ../nxfs-defs.nix;
@@ -24,7 +23,6 @@ derivation {
   system       = builtins.currentSystem;
 
   toolchain    = nxfs-toolchain-1;
-  sysroot      = nxfs-sysroot-1;
   coreutils    = nxfs-coreutils-1;
   gnumake      = nxfs-gnumake-1;
   gawk         = nxfs-gawk-1;
@@ -42,8 +40,6 @@ derivation {
   args         = [ ./builder.sh ];
 
   src          = builtins.fetchTarball { name = "bash-5.2.32-source";
-                                         url = "https://ftp.gnu.org/gnu/bash/bash-5.2.32.tar.gz";
+                                         url = "https://ftpmirror.gnu.org/gnu/bash/bash-5.2.32.tar.gz";
                                          sha256 = "1bhqakwia1zpnq9kgpn7kxsgvgh5b8nysanki0j2m7v7im4yjcvp"; };
-
-  target_tuple = nxfs-defs.target_tuple;
 }
