42a1958840dcea4417c057d9cc06e3b4d6a4ad09
Roland Conybeare <rconybeare@gmail.com>
2025-10-12 01:27:16 -0400

    nxfs-autoconf-2: -euo pipefail + direct toolchain + streamline

--- nxfspkgs/bootstrap-2/nxfs-autoconf-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-autoconf-2/default.nix
@@ -13,7 +13,6 @@ let
   nxfs-toolchain-wrapper-1 = import ../../bootstrap-1/nxfs-toolchain-wrapper-1/default.nix;
 
   nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
-  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
 
   nxfs-defs = import ../nxfs-defs.nix;
 in
@@ -38,15 +37,11 @@ derivation {
   gcc_wrapper  = nxfs-toolchain-wrapper-1;
 
   toolchain    = nxfs-toolchain-1;
-  sysroot      = nxfs-sysroot-1;
 
   builder      = "${nxfs-bash-2}/bin/bash";
   args         = [ ./builder.sh ];
 
-  #src         = nxfs-sed-source;
   src          = builtins.fetchTarball { name = "autoconf-2.72-source";
-                                         url = "https://ftp.gnu.org/gnu/autoconf/autoconf-2.72.tar.xz";
+                                         url = "https://ftpmirror.gnu.org/gnu/autoconf/autoconf-2.72.tar.xz";
                                          sha256 = "1r3922ja9g5ziinpqxgfcc51jhrxvjqnrmc5054jgskylflxc1fp"; };
-
-  target_tuple = nxfs-defs.target_tuple;
 }
