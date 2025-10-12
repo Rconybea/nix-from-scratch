830b9c7c73538b879289c6b7f283eec86290590a
Roland Conybeare <rconybeare@gmail.com>
2025-10-12 01:29:23 -0400

    nxfs-texinfo-2: -euo pipefail + direct toolchain + streamline

--- nxfspkgs/bootstrap-2/nxfs-texinfo-2/builder.sh
+++ nxfspkgs/bootstrap-2/nxfs-texinfo-2/builder.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e
-set -x
+set -euo pipefail
 
 echo "automake=${automake}"
 echo "autoconf=${autoconf}"
@@ -15,67 +14,31 @@ echo "gawk=${gawk}"
 echo "grep=${grep}"
 echo "sed=${sed}"
 echo "tar=${tar}"
-echo "sysroot=${sysroot}"
 echo "gcc_wrapper=${gcc_wrapper}"
 echo "src=${src}"
 echo "toolchain=${toolchain}"
-echo "target_tuple=${target_tuple}"
 echo "TMPDIR=${TMPDIR}"
 
-# 1. ${gcc_wrapper}/bin/x86_64-pc-linux-gnu-{gcc,g++} builds viable executables.
-# 2. ${toolchain}/bin/x86_64-pc-linux-gnu-gcc can build executables,
-#    but they won't run unless we pass special linker flags
-# 3. ${toolchain}/bin                     has x86_64-pc-linux-gnu-ar
-# 4. ${toolchain}/x86_64-pc-linux-gnu/bin has ar  <- autotools looks for this
-#
-export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${automake}/bin:${autoconf}/bin:${m4}/bin:${perl}/bin:${file}/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${findutils}/bin:${diffutils}/bin:${bash}/bin"
-
-ls -l ${toolchain}/x86_64-pc-linux-gnu/bin
+set -x
+export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${automake}/bin:${autoconf}/bin:${m4}/bin:${perl}/bin:${file}/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${findutils}/bin:${diffutils}/bin:${bash}/bin"
 
 src2=${TMPDIR}/src2
 builddir=${TMPDIR}/build
 
-mkdir -p ${src2}
 mkdir -p ${builddir}
 
 mkdir ${out}
 
 bash_program=${bash}/bin/bash
-file_program=${file}/bin/file
-
-# 1. copy source tree to temporary directory,
-#
-(cd ${src} && (tar cf - . | tar xf - -C ${src2}))
-
-# 2. substitute nix-store path-to-bash for /bin/sh.
-#
-#
-chmod -R +w ${src2}
-# note: text replacement of /usr/bin/file here causes build to need m4,automake, triggers build failure further downstream.
-#(cd ${src2} && (find . -type f | xargs --replace=xx sed -i -e "s:/usr/bin/file:${file_program}:" xx))
-
-# need writeable for autom4te cache
-#chmod -R -w ${src2}
-
-# Must skip:
-#   .m4 and .in files (assume they trigger re-running autoconf)
-#   test/ files
-#
-#sed -i -e "s:/bin/sh:${bash_program}:g" ${src2}/configure #${src2}/build-aux/*
 
 # ${src}/configure honors CONFIG_SHELL
 export CONFIG_SHELL="${bash_program}"
 
-# 1.
-# we shouldn't need special compiler/linker instructions,
-# since stage-1 toolchain "knows where it lives"
-#
-# 2.
-# do need to give --host and --build arguments to configure,
-# since we're using a cross compiler.
-
-(cd ${builddir} && bash ${src2}/configure --prefix=${out} --host=${target_tuple} --build=${target_tuple} CFLAGS="-I${sysroot}/usr/include" LDFLAGS="-Wl,-enable-new-dtags")
+(cd ${builddir} && bash ${src}/configure --prefix=${out} CC=nxfs-gcc LDFLAGS="-Wl,-enable-new-dtags")
 
 (cd ${builddir} && make SHELL=${CONFIG_SHELL})
 
 (cd ${builddir} && make install SHELL=${CONFIG_SHELL})
+
+# verify an executable runs
+${out}/bin/makeinfo --version
