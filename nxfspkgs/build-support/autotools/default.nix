11bfb0f2e791723e697f06d6c249a52ea9c565e2
Roland Conybeare <rconybeare@gmail.com>
2025-10-12 15:49:47 -0400

    cosmetic: tidy comments in autotools builder

--- nxfspkgs/build-support/autotools/default.nix
+++ nxfspkgs/build-support/autotools/default.nix
@@ -1,6 +1,12 @@
-
-# pkgs  :: attrset   nxfs package repository nix-from-scratch/nxfspkgs, possibly extended
-nxfsenv : #pkgs :
+# nxfsenv :: attrset   nxfs package repository nix-from-scratch/nxfspkgs, possibly extended
+#
+# Require:
+#   nxfsenv.coreutils :: derivation. Suitable version of (probably gnu) coreutils
+#                        Expect ${nxfsenv.coreutils}/bin to provide at bare minimum
+#                        {mkdir, cp, rm, chmod, nproc}; probably others.
+#   nxfsenv.bash      :: derivation. With ${nxfsenv.bash}/bin/bash
+#
+nxfsenv :
 
 # this function (rest of this file)
 #   :: attrset->derivation will be invoked as nxfsenv.mkDerivation
@@ -9,7 +15,6 @@ nxfsenv : #pkgs :
 attrs :
 
 let
-#  nxfsenv = pkgs.nxfsenv;
   bash = nxfsenv.bash;
 in
 
@@ -22,13 +27,10 @@ let
 
     # baseInputs :: list(derivation)   elt d[i] passed to builder as nix-store path
     baseInputs = [ nxfsenv.coreutils ];
-#    baseInputs = with pkgs; [
-#      nxfs-bootstrap-1.nxfs-coreutils-1
-#    ];
     # buildInputs :: list(derivation)  elt d[i] passed to builder as nix-store path
     buildInputs = [ ];
 
-    # inputs that should also be preserved for runtime
+    # inputs that should also be preserved for runtime.
     propagatedBuildInputs = [ ];
 
     system = builtins.currentSystem;
