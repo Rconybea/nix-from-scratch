19cf9c81c7eb224f56f84d85d468df1921b3f645
Roland Conybeare <rconybeare@gmail.com>
2025-10-12 00:46:28 -0400

    nxfs-grep-2: -euo pipefail + direct toolchain + streamline

--- nxfspkgs/bootstrap-2/nxfs-grep-2/builder.sh
+++ nxfspkgs/bootstrap-2/nxfs-grep-2/builder.sh
@@ -1,68 +1,38 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 echo "gcc_wrapaper=${gcc_wrapper}"
 echo "toolchain=${toolchain}"
 echo "gnumake=${gnumake}"
 echo "gawk=${gawk}"
-echo "grep=${grep}"
-echo "sed=${sed}"
-echo "tar=${tar}"
+echo "gnugrep=${gnugrep}"
+echo "gnused=${gnused}"
+echo "gnutar=${gnutar}"
 echo "coreutils=${coreutils}"
 echo "findutils=${findutils}"
 echo "diffutils=${diffutils}"
-echo "sysroot=${sysroot}"
-#echo "mkdir=${mkdir}"
-#echo "head=${head}"
 echo "bash=${bash}"
 echo "src=${src}"
-echo "target_tuple=${target_tuple}"
 echo "TMPDIR=${TMPDIR}"
 
-# 1. ${gcc_wrapper}/bin/x86_64-pc-linux-gnu-{gcc,g++} builds viable executables.
-# 2. ${toolchain}/bin/x86_64-pc-linux-gnu-gcc can build executables,
-#    but they won't run unless we pass special linker flags
-# 3. ${toolchain}/bin                     has x86_64-pc-linux-gnu-ar
-# 4. ${toolchain}/x86_64-pc-linux-gnu/bin has ar  <- autotools looks for this
-#
-export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${toolchain}/x86_64-pc-linux-gnu/bin:${gnumake}/bin:${gawk}/bin:${grep}/bin:${sed}/bin:${tar}/bin:${coreutils}/bin:${diffutils}/bin:${findutils}/bin:${bash}/bin"
-
-ls -l ${toolchain}/x86_64-pc-linux-gnu/bin
+export PATH="${gcc_wrapper}/bin:${toolchain}/bin:${gnumake}/bin:${gawk}/bin:${gnugrep}/bin:${gnused}/bin:${gnutar}/bin:${coreutils}/bin:${diffutils}/bin:${findutils}/bin:${bash}/bin"
 
 builddir=${TMPDIR}
-#tmpsrcdir=${TMPDIR}/src
-#mkdir -p ${tmpsrcdir}
-#builddir=${TMPDIR}/build
-#mkdir -p ${builddir}
 
 mkdir ${out}
-
-# 1. copy source tree to temporary directory,
-#
-#(cd ${src} && (tar cf - . | tar xf - -C ${tmpsrcdir}))
-
-# 2. substitute nix-store path-to-bash for /bin/sh throughout
-#
 
 bash_program=${bash}/bin/bash
 
 # ${src}/configure honors CONFIG_SHELL
 export CONFIG_SHELL="${bash_program}"
 
-# 1.
-# we shouldn't need special compiler/linker instructions,
-# since stage-1 toolchain "knows where it lives"
-#
-# 2.
-# do need to give --host and --build arguments to configure,
-# since we're using a cross compiler.
-
 # inspect shebang
 #head -5 ${src}/configure
 
-#(cd ${builddir} && bash ${src}/configure --prefix=${out} --host=${target_tuple} --build=${target_tuple} CFLAGS="-I${sysroot}/usr/include" LDFLAGS="-Wl,--enable-new-dtags -Wl,-rpath,${sysroot}/lib -Wl,-dynamic-linker,${sysroot}/lib/ld-linux-x86-64.so.2)")
-(cd ${builddir} && ${bash_program} ${src}/configure --prefix=${out} --host=${target_tuple} --build=${target_tuple} CFLAGS="-I${sysroot}/usr/include" LDFLAGS="-Wl,-enable-new-dtags")
+(cd ${builddir} && ${bash_program} ${src}/configure --prefix=${out} CC="nxfs-gcc" CPP=cpp CPPFLAGS="-I${toolchain}/include" CFLAGS="-I${toolchain}/include" LDFLAGS="-Wl,-enable-new-dtags")
 
 (cd ${builddir} && make SHELL=${CONFIG_SHELL})
 (cd ${builddir} && make install SHELL=${CONFIG_SHELL})
+
+${out}/bin/grep --version
--- nxfspkgs/bootstrap-2/nxfs-grep-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-grep-2/default.nix
@@ -1,21 +1,20 @@
 let
-  nxfs-grep-1        = import ../../bootstrap-1/nxfs-grep-1/default.nix;
+  nxfs-gnugrep-1           = import ../../bootstrap-1/nxfs-grep-1/default.nix;
 
   nxfs-toolchain-wrapper-1 = import ../../bootstrap-1/nxfs-toolchain-wrapper-1/default.nix;
 
-  nxfs-gnumake-1     = import ../../bootstrap-1/nxfs-gnumake-1/default.nix;
-  nxfs-gawk-1        = import ../../bootstrap-1/nxfs-gawk-1/default.nix;
-  nxfs-tar-1         = import ../../bootstrap-1/nxfs-tar-1/default.nix;
-  nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
-  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
-  nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
-  nxfs-bash-1        = import ../../bootstrap-1/nxfs-bash-1/default.nix;
+  nxfs-gnumake-1           = import ../../bootstrap-1/nxfs-gnumake-1/default.nix;
+  nxfs-gawk-1              = import ../../bootstrap-1/nxfs-gawk-1/default.nix;
+  nxfs-gnutar-1            = import ../../bootstrap-1/nxfs-tar-1/default.nix;
+  nxfs-toolchain-1         = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
+  nxfs-coreutils-1         = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
+  nxfs-bash-1              = import ../../bootstrap-1/nxfs-bash-1/default.nix;
 
-  nxfs-sed-2         = import ../nxfs-sed-2/default.nix;
-  nxfs-findutils-2   = import ../nxfs-findutils-2/default.nix;
-  nxfs-diffutils-2   = import ../nxfs-diffutils-2/default.nix;
+  nxfs-gnused-2            = import ../nxfs-sed-2/default.nix;
+  nxfs-findutils-2         = import ../nxfs-findutils-2/default.nix;
+  nxfs-diffutils-2         = import ../nxfs-diffutils-2/default.nix;
 
-  nxfs-defs = import ../nxfs-defs.nix;
+  nxfs-defs                = import ../nxfs-defs.nix;
 in
 
 derivation {
@@ -25,13 +24,12 @@ derivation {
   gnumake      = nxfs-gnumake-1;
   bash         = nxfs-bash-1;
   toolchain    = nxfs-toolchain-1;
-  sysroot      = nxfs-sysroot-1;
   coreutils    = nxfs-coreutils-1;
-  tar          = nxfs-tar-1;
+  gnutar       = nxfs-gnutar-1;
   gawk         = nxfs-gawk-1;
-  grep         = nxfs-grep-1;
+  gnugrep      = nxfs-gnugrep-1;
 
-  sed          = nxfs-sed-2;
+  gnused       = nxfs-gnused-2;
   findutils    = nxfs-findutils-2;
   diffutils    = nxfs-diffutils-2;
 
@@ -41,8 +39,6 @@ derivation {
   args         = [ ./builder.sh ];
 
   src          = builtins.fetchTarball { name = "grep-3.11-source";
-                                         url = "https://ftp.gnu.org/gnu/grep/grep-3.11.tar.xz";
+                                         url = "https://ftpmirror.gnu.org/gnu/grep/grep-3.11.tar.xz";
                                          sha256 = "0pm0zpzmmy6lq5ii03y1nqr1sdjalnwp69i5c926c9dm03v7v0bv"; };
-
-  target_tuple = nxfs-defs.target_tuple;
 }
