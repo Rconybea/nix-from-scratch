1ea797148b217f8a8bdc47577ddd2f6fd14e4720
Roland Conybeare <rconybeare@gmail.com>
2025-10-12 00:47:05 -0400

    nxfs-tar-2: direct toolchain + streamlining + set -euo pipefail

--- nxfspkgs/bootstrap-2/nxfs-tar-2/default.nix
+++ nxfspkgs/bootstrap-2/nxfs-tar-2/default.nix
@@ -3,10 +3,9 @@ let
   nxfs-gawk-1        = import ../../bootstrap-1/nxfs-gawk-1/default.nix;
 
   nxfs-toolchain-1   = import ../../bootstrap-1/nxfs-toolchain-1/default.nix;
-  nxfs-sysroot-1     = import ../../bootstrap-1/nxfs-sysroot-1/default.nix;
   nxfs-coreutils-1   = import ../../bootstrap-1/nxfs-coreutils-1/default.nix;
   nxfs-bash-1        = import ../../bootstrap-1/nxfs-bash-1/default.nix;
-  nxfs-tar-1         = import ../../bootstrap-1/nxfs-tar-1/default.nix;
+  nxfs-gnutar-1      = import ../../bootstrap-1/nxfs-tar-1/default.nix;
 
 
   nxfs-grep-2        = import ../nxfs-grep-2/default.nix;
@@ -19,18 +18,17 @@ let
 in
 
 derivation {
-  name         = "nxfs-tar-2";
+  name         = "nxfs-gnutar-2";
 
   system       = builtins.currentSystem;
 
   bash         = nxfs-bash-1;
   toolchain    = nxfs-toolchain-1;
-  sysroot      = nxfs-sysroot-1;
   coreutils    = nxfs-coreutils-1;
 
   gnumake      = nxfs-gnumake-1;
   gawk         = nxfs-gawk-1;
-  tar          = nxfs-tar-1;
+  gnutar       = nxfs-gnutar-1;
 
   grep         = nxfs-grep-2;
   sed          = nxfs-sed-2;
@@ -43,8 +41,6 @@ derivation {
 
   #src         = nxfs-sed-source;
   src          = builtins.fetchTarball { name = "tar-1.35-source";
-                                         url = "https://ftp.gnu.org/gnu/tar/tar-1.35.tar.xz";
+                                         url = "https://ftpmirror.gnu.org/gnu/tar/tar-1.35.tar.xz";
                                          sha256 = "0cmdg6gq9v04631lfb98xg45la1b0y9r5wyspn97ri11krdlyfqz"; };
-
-  target_tuple = nxfs-defs.target_tuple;
 }
