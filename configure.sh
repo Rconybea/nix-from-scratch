a729085eae04ff3dc84a267fb866882d158acb4d
Roland Conybeare <rconybeare@gmail.com>
2025-10-26 14:08:43 -0400

    configure: better help + prep nxfs-defs.nix + github workflow
    1. gihub workflow uses ./configure.  Retire ./bootstrap.sh
    2. generate nix-defs.nix from ./configure.  Not used yet.
    3. improve ./configure help message + support max-jobs

--- configure.sh
+++ configure.sh
@@ -2,12 +2,59 @@
 
 set -euo pipefail
 
+self_name=$(basename ${0})
+
+usage() {
+    cat <<EOF
+$self_name: [--prefix=PREFIX] [--nxfs-toolchain-prefix=NXFS_TOOLCHAIN_PREFIX] [--nxfs-max-jobs=NXFS_MAX_JOBS]
+
+PREFIX    install nixcpp dependencies (boost, bison, rustc, ..) to this directory
+          [$HOME/ext]
+
+NXFS_TOOLCHAIN_PREFIX
+          install standaline {gcc, glibc} toolchain to this directory
+          Distinct from PREFIX so it can be easily discarded.
+          Desirable because install involves multiple steps, across which
+          NXFS_TOOLCHAIN_PREFIX passes through unusable states.
+          [$HOME/nxfs-toolchain]
+
+NXFS_MAX_JOBS
+          drives max number of parallel jobs within a build
+          (-j argument to make). Note that link time optimization is memory
+          hungry; packages that prepare many exectables can wake up OOM killer.
+          Watch out for this with llvm in particular.
+          [`nproc`]
+
+EOF
+}
+
 PREFIX=${HOME}/ext
-NIX_PREFIX=$(realpath ${HOME}/nixroot)
 NXFS_TOOLCHAIN_PREFIX=${HOME}/nxfs-toolchain
+NIX_PREFIX=$(realpath ${HOME}/nixroot)
 NXFS_HOST_TUPLE=x86_64-pc-linux-gnu
 NXFS_BUILD_TUPLE=x86_64-pc-linux-gnu
 NXFS_MAX_JOBS=$(nproc)
+
+while [[ $# > 0 ]]; do
+    case "$1" in
+        --prefix=*)
+            PREFIX="${1#*=}"
+            ;;
+        --nxfs-toolchain-prefix=*)
+            NXFS_TOOLCHAIN_PREFIX="${1#*=}"
+            ;;
+        --nxfs-max-jobs=*)
+            NXFS_MAX_JOBS="${1#*=}"
+            ;;
+        *)
+            echo "error: ${self_name}: unexpected argument [$1]"
+            usage
+            exit 1
+            ;;
+    esac
+
+    shift
+done
 
 # adopt config vars into makefiles for nixcpp + nixcpp deps
 sed -e s:@prefix@:${PREFIX}: \
@@ -22,8 +69,18 @@ sed -e s:@prefix@:${PREFIX}: \
 # (PREFIX not needed -> omitted deliberately)
 #
 cat <<EOF > nxfspkgs/bootstrap/nxfs-vars.sh
+# DO NOT EDIT: created automatically by nix-from-scratch/configure.sh
+
 NIX_PREFIX=${NIX_PREFIX}
 NXFS_TOOLCHAIN_PREFIX=${NXFS_TOOLCHAIN_PREFIX}
 NXFS_HOST_TUPLE=${NXFS_HOST_TUPLE}
 NXFS_MAX_JOBS=${NXFS_MAX_JOBS}
+EOF
+
+cat <<EOF > nxfspkgs/bootstrap/nxfs-defs.nix
+# DO NOT EDIT: created automatically by nix-from-scratch/configure.sh
+{
+  system = "x86_64-linux";
+  target-tuple = "${NXFS_BUILD_TUPLE}";
+}
 EOF
